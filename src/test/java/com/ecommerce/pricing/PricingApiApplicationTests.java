bdbdaebb29bd00ce977bb6eccf5072b83b97cd96
Nandan <Nandan1981@gmail.com>
2023-08-21 00:10:37 +0530

    code for pricing service

---
+++ src/test/java/com/ecommerce/pricing/PricingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
