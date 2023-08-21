63650418d730de06376fb8091fbe3ff446a0b573
Nandan <Nandan1981@gmail.com>
2023-08-21 17:14:06 +0530

    minor changes

--- src/main/java/com/ecommerce/pricing/controller/PricingController.java
+++ src/main/java/com/ecommerce/pricing/controller/PricingController.java
@@ -1,12 +1,10 @@
 package com.ecommerce.pricing.controller;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.ServerRequest.Headers;
 
 @RestController
 @RequestMapping("/pricing")
