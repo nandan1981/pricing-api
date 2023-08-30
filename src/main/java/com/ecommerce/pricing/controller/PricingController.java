da87c5ac6ea33e207ba0ca8a83e083bd0e725460
Nandan <Nandan1981@gmail.com>
2023-08-30 21:29:44 +0530

    sonar way change

--- src/main/java/com/ecommerce/pricing/controller/PricingController.java
+++ src/main/java/com/ecommerce/pricing/controller/PricingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pricing")
+@RequestMapping("/v1/pricing")
 public class PricingController {
 
 	@GetMapping("/helloWorld")
