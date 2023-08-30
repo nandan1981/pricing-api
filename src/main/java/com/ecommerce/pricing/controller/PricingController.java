f01fa4a0ec8ea0df2cfa98af3f968f996ef0c816
Nandan <Nandan1981@gmail.com>
2023-08-31 01:18:34 +0530

    logger added to the project

--- src/main/java/com/ecommerce/pricing/controller/PricingController.java
+++ src/main/java/com/ecommerce/pricing/controller/PricingController.java
@@ -1,14 +1,19 @@
 package com.ecommerce.pricing.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/v1/pricing")
 public class PricingController {
+	
+	private static final Logger logger = LogManager.getLogger(PricingController.class);
 
 	@GetMapping("/helloWorld")
 	public String helloWorld() {
@@ -17,6 +22,8 @@ public class PricingController {
 	
 	@GetMapping("/getQuotation")
 	public ResponseEntity<String> getQuotation(){
+		
+		logger.info("getQuotation");
 		HttpHeaders headers = new HttpHeaders();
 		return ResponseEntity.ok().headers(headers).body("12.44");
 	}
